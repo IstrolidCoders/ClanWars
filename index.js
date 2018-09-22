17674bbc624d736f7833ddbb106c4366b8a26a78
Rio6 <rio6.liu@gmail.com>
2018-09-22 00:22:46 -0700

    Basic web server

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var {IstrolidClient} = require('./IstrolidClient.js');
+
+const PORT = 80;
+
+var app = express();
+
+var istroClients = [];
+var globalServer = new IstrolidClient();
+
+app.get('/istrolid', (req, res) => {
+    res.send(istroClients);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(req.path, {root: 'html'});
+});
+
+var server = app.listen(PORT, function () {
+    console.log("Listening on port", server.address().port);
+});
+
+globalServer.on('server', s => {
+    if(!istroClients[s.name]) {
+        let client = new IstrolidClient(s.name);
+        client.connect();
+        istroClients.push(client);
+    }
+});
+
+globalServer.connect();
