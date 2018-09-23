8a2616239f5b20479782d0ec1e86526cd226efcc
Rio6 <rio6.liu@gmail.com>
2018-09-22 21:01:13 -0700

    DB test

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var {IstrolidClient} = require('./IstrolidClient.js');
+var pg = require('pg');
 
 const PORT = process.env.PORT || 8080;
 
-var app = express();
+var db = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+db.connect().then(() => console.log("db connected"));
 
+var app = express();
 var istroClients = [];
 var globalServer = new IstrolidClient();
 
 app.get('/istrolid', (req, res) => {
-    res.send(istroClients);
+    db.query('SELECT * from BattleRecords;', (err, res) => {
+        if(err) throw err;
+        let r = "";
+        for(let row of res.rows) {
+            r += JSON.stringify(row) + "<br/>";
+        }
+        res.send(r);
+    });
 });
 
 app.get('/', (req, res) => {
@@ -23,6 +36,14 @@ var server = app.listen(PORT, function () {
 globalServer.on('server', s => {
     if(!istroClients[s.name]) {
         let client = new IstrolidClient(s.name);
+        client.on('gameended', e => {
+            db.query('INSERT INTO BattleRecords (server, type, players, win) VALUES ($1, $2, $3::json[], $4);',
+                [client.serverName, client.serverType, e.players.map(p => JSON.stringify(p)), e.win],
+                (err, res) => {
+                    if(err) throw err;
+                });
+        });
+
         client.connect();
         istroClients.push(client);
     }
